f2aa063f7b1f6f3770cd6ae94add84b437d8220a
walterrodriguesjr <walterrjr.86@gmail.com>
2024-08-06 17:24:37 -0300

    atualizando

--- app/App.tsx
+++ app/App.tsx
@@ -1,14 +1,51 @@
 
-import { StatusBar } from 'react-native';
-import React from 'react';
+import { StatusBar, TextInput } from 'react-native';
+import React, { useState } from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return <SafeAreaView style={styles.container}>
-    
+
+  const [newTask, setNewTask] = useState("");
+
+  return <SafeAreaView
+    style={styles.container}>
+    <Text style={styles.title}>
+      Adicione uma nova tarefa
+    </Text>
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.input}
+        placeholder='Digite aqui...'
+        onChangeText={(text) => setNewTask(text)}
+        value={newTask}>
+        
+
+      </TextInput>
+    </View>
   </SafeAreaView>
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#F5F5F5",
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 5,
+    color: "#333"
+  },
+  inputContainer: {
+    flexDirection: "row",
+    marginBottom: 20
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    fontSize: 18,
+    borderRadius: 5,
+    marginRight: 10
+  }
 });
