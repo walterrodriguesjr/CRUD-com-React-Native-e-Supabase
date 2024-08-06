d2157a5bbac760380c8da707781ea8853441cefa
Walter Rodrigues Junior <walter.junior00@pm.pr.gov.br>
2024-08-06 13:24:14 -0300

    atualizando

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,14 @@
+
+import { StatusBar } from 'react-native';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to the App screen!</Text>
-    </View>
-  );
+  return <SafeAreaView style={styles.container}>
+    
+  </SafeAreaView>
 }
+
+const styles = StyleSheet.create({
+  container: {},
+});
